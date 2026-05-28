074d29ab0a11a1cf4ea1bdc2f224337501848685
Shobha722 <chimpireddyshobharani@gmail.com>
2026-05-28 12:55:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
-const API = "http://localhost:8080/api/arts";
+import Home from "./pages/Home";
 
-async function loadArts() {
-
-    const response = await fetch(API);
-
-    const arts = await response.json();
-
-    const feed = document.getElementById("artFeed");
-
-    feed.innerHTML = "";
-
-    arts.forEach(art => {
-
-        feed.innerHTML += `
-            <div class="col-md-4 mb-4">
-                <div class="card">
-
-                    <img src="${art.imageUrl}" class="card-img-top">
-
-                    <div class="card-body">
-                        <h4 class="art-title">${art.title}</h4>
-
-                        <p>${art.description}</p>
-
-                        <button class="like-btn">
-                            ❤️ Like
-                        </button>
-                    </div>
-                </div>
-            </div>
-        `;
-    });
+function App() {
+  return <Home />;
 }
 
-loadArts();
+export default App;
